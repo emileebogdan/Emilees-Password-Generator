373fca861315c93d083516b6279edbb89d66b32e
emileebogdan <emileebogdan@gmail.com>
2022-07-02 12:20:38 -0600

    final

---
+++ Develop/script.js
@@ -0,0 +1,63 @@
+var characterLength = 8;
+var choiceArray = [];
+var specCharArray = ['!','@','#','$','%','^','&','*','(',')','-','_','+','=','`','~','<','>','/','?'];
+var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','h','j','k','i','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numberArray = ['0','1','2','3','4','5','6','7','8','9'];
+
+var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+  var correctPrompts = Prompts();
+  var passwordText = document.querySelector("#password");
+
+  if(correctPrompts){
+    var newPassword = generatePassword ();
+    passwordText.value = newPassword;
+   } else {
+    passwordText.value = "";
+   }
+
+}
+
+function generatePassword() {
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomLetter = Math.floor(Math.random() * choiceArray.length);
+    password = password + choiceArray[randomLetter];
+  }
+  return password;
+}
+
+function Prompts(){
+  choiceArray = [];
+  characterLength = parseInt(prompt("How Many Characters do you want your Password to be? (Your Password must be Between 8 and 126 Characters."));
+  if (isNaN (characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Your Password Must be Between 8 and 126 Characters. Please Try Again.");
+    return false;
+  }
+
+  if (confirm("Would you like to add lower case letters in your password?")){
+    choiceArray = choiceArray.concat(lowerCaseArray);
+  }
+
+  if (confirm("Would you like to add upper case letters in your password?")){
+    choiceArray = choiceArray.concat(upperCaseArray);
+  }
+
+  if (confirm("Would you like to special characters in your password?")){
+    choiceArray = choiceArray.concat(specCharArray);
+  }
+
+  if (confirm("Would you like to add numbers in your password?")){
+    choiceArray = choiceArray.concat(numberArray);
+  }
+ 
+  return true;
+}
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Emilees-Password-Generator
+
+## Description
+
+Edited js code to enable the webpage to return a password based on prompts. Password is random, has between 8 and 128 characters, special characters, capital letters, lowercase letters, and numbers. 
+
+## Credits
+
+Emilee Bogdan
+
+-[Git Hub](https://github.com/emileebogdan)
+
+-[LinkedIn](https://www.linkedin.com/in/emileebogdan/)
+
+
+<img src = images/screenshot.png>
